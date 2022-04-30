3fcce84ff37f22746454225bfa7f7971eb1c9e62
RinaKovri <ekaterina.romanna@gmail.com>
2022-04-30 20:59:46 +0300

    changed the scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,37 +8,42 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     private Rigidbody rb;
     private AudioSource playerAudio;
+    public float moveSpeed;
     //public ParticleSystem explosionParticle;
     //public ParticleSystem dirtParticle;
     //public AudioClip attackSound;
     //public AudioClip deadSound;
-   
+
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        playerAudio = GetComponent<AudioSource>();
-    }
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    playerAudio = GetComponent<AudioSource>();
+    //}
 
     // Update is called once per frame
-    void Update()
-    {  
-           
-    }
-        
-    private void OnTriggerEnter(Collider other)
+    void FixedUpdate()
     {
-        if(other.gameObject.CompareTag("PickUp"))
-        {
-            Destroy(other.gameObject);
-            //explosionParticle.Play();
-        }
-    
-        if (other.gameObject.CompareTag("Obstacle"))
-        {
-            anim.SetTrigger("dead");
-            Debug.Log("Game Over");
-            gameOver = true;
-        }
+        MoveObj();
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.CompareTag("PickUp"))
+    //    {
+    //        Destroy(other.gameObject);
+    //        //explosionParticle.Play();
+    //    }
+
+    //    if (other.gameObject.CompareTag("Obstacle"))
+    //    {
+    //        anim.SetTrigger("dead");
+    //        Debug.Log("Game Over");
+    //        gameOver = true;
+    //    }
+    //}
+    void MoveObj()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
     }
 }
