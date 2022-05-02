9ad5680a431406dcd7c9b6a4d436ba2e354d2243
RinaKovri <ekaterina.romanna@gmail.com>
2022-05-02 16:18:40 +0300

    added new pickups, fixed PC script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,45 +5,56 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public bool gameOver;
-    public Animator anim;
+    public Animation anim;
     private Rigidbody rb;
     private AudioSource playerAudio;
     public float moveSpeed;
-    //public ParticleSystem explosionParticle;
-    //public ParticleSystem dirtParticle;
-    //public AudioClip attackSound;
-    //public AudioClip deadSound;
+    public float jumpForce;
+    public AudioClip starSound;
+    public float xRange = 1.2f;
 
     // Start is called before the first frame update
-    //void Start()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //    playerAudio = GetComponent<AudioSource>();
-    //}
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+        playerAudio = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        MoveObj();
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * moveSpeed);
+        Move();
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if(other.gameObject.CompareTag("PickUp"))
-    //    {
-    //        Destroy(other.gameObject);
-    //        //explosionParticle.Play();
-    //    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            Destroy(other.gameObject);
+            playerAudio.PlayOneShot(starSound, 1.0f);
+        }
 
-    //    if (other.gameObject.CompareTag("Obstacle"))
-    //    {
-    //        anim.SetTrigger("dead");
-    //        Debug.Log("Game Over");
-    //        gameOver = true;
-    //    }
-    //}
-    void MoveObj()
+       
+    }
+    void Move()
     {
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (AnimationState Runtojumpspring in anim);
+            //rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
     }
 }
